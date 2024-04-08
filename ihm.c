f53eedc4184d059d850b0a6079760d8fedaa2d9a
MillianLMX <millian59192@gmail.com>
2024-04-08 13:53:01 +0200

    Adding initial IHM elements in order to create the base of the Puissance4 project.

---
+++ ihm.c
@@ -0,0 +1,68 @@
+#include "ihm.h"
+#include "colors.h"
+
+void afficherPlateau(int **matrix, int col, int ligne)
+{
+    char separator[] = "##########";
+
+    char *figure[] = {
+        "██████╗",
+        "██╔═══██╗",
+        "██║   ██║",
+        "██║   ██║",
+        "╚██████╔╝",
+        "╚═════╝"};
+
+    char *emptyFigure[] = {
+        "       ",
+        "         ",
+        "         ",
+        "         ",
+        "         ",
+        "       ",
+    };
+
+    char *delimiter[] = {
+        "###",
+        "#",
+        "#",
+        "#",
+        "#",
+        "###",
+    };
+
+    char *baseDelimiter[] = {
+        "##",
+        "#",
+        "#",
+        "#",
+        "#",
+        "##",
+    };
+
+    for (int i = 0; i < ligne; i++)
+    { // for each line
+        for (int l = 0; l < 7; l++)
+        { // for line of figure
+            for (int j = 0; j < col; j++)
+            { // for each token
+                if (l == 0)
+                    printf("%s", separator);
+                else
+                {
+                    const char *color = matrix[i][j] == 1 ? RED : YELLOW;
+                    printf("%s%s%s%s%s", j == 0 ? baseDelimiter[l - 1] : delimiter[l - 1], color, matrix[i][j] != 0 ? figure[l - 1] : emptyFigure[l - 1], WHITE, j == col - 1 ? baseDelimiter[l - 1] : "");
+                }
+
+                if (l == 0 && j == col - 1)
+                {
+                    printf("#");
+                }
+            }
+            printf("\n");
+        }
+    }
+    for (int l = 0; l < col; l++)
+        printf("%s", separator);
+    printf("#\n");
+}
---
+++ ihm.h
@@ -0,0 +1,64 @@
+/**
+ *	\file		ihm.h
+ *	\brief		Spécification de la couche IHM
+ *	\author		Millian Lamiaux & Malo Garnier
+ *	\date		8 avril 2024
+ *	\version	1.0
+ */
+
+#ifndef IHM_H
+#define IHM_H
+
+/*
+*****************************************************************************************
+ *	\noop		I N C L U D E S   S P E C I F I Q U E S
+ */
+#include <stdio.h>
+#include "data.h"
+
+/*
+*****************************************************************************************
+ *	\noop		D E F I N I T I O N   DES   M A C R O S
+ */
+/**
+ *	\def		CHECK(sts, msg)
+ *	\brief		Macro-fonction qui vérifie que sts est égal -1 (cas d'erreur : sts==-1) 
+ *				En cas d'erreur, il y a affichage du message adéquat et fin d'exécution  
+ */
+#define CHECK(sts, msg) if ((sts)==-1) {perror(msg); exit(-1);}
+/**
+ *	\def		PAUSE(msg)
+ *	\brief		Macro-fonction qui affiche msg et attend une entrée clavier  
+ */
+#define PAUSE(msg)	printf("%s [Appuyez sur entrée pour continuer]", msg); getchar();
+/*
+*****************************************************************************************
+ *	\noop		S T R C T U R E S   DE   D O N N E E S
+ */
+/**
+ *	\struct		socket
+ *	\brief		Définition de la structure de données socket
+ *	\note		Ce type est composé du fd de la socket, du mode (connecté/non)
+ *				et des adresses applicatives (locale/distante)
+ */
+struct requete_t {
+	int fd;							/**< numéro de la socket créée			*/
+	int mode;						/**< mode connecté/non : STREAM/DGRAM	*/
+	struct sockaddr_in addrLoc;		/**< adresse locale de la socket 		*/
+	struct sockaddr_in addrDst;		/**< adresse distante de la socket 		*/
+};
+/*
+*****************************************************************************************
+ *	\noop		P R O T O T Y P E S   DES   F O N C T I O N S
+ */
+/**
+ *	\fn			void afficherPlateau (int **matrix, int col, int ligne)
+ *	\brief		Affiche la matrice du Puissance 4 au format humain dans le Shell
+ *	\param		matrix : matrice d'entier comportant des 0, 1 ou 2
+ *	\param		col : le nombre de colonnes de la matrice
+ *	\param		ligne : le nombre de lignes de la matrice
+ *	\note		Ne pas donner une matrice trop grande sous peine de dégrader l'expérience utilisateur
+ */
+void afficherPlateau(int **matrix, int col, int ligne);
+
+#endif /* IHM_H */
